bbb34975b2bf10de22cc2024e251f295dacc5324
macenuw <gnykabiny54@gamil.com>
2022-02-09 17:15:36 +0200

    finished desctop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
-$(function(){
+$(function () {
+  $('.select').styler();
+  $('.recently__slider').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
 
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          centerPadding: 50,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
 })
